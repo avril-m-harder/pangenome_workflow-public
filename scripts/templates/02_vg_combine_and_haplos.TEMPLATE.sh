a3e707c40ae0071e81db1bfa30c06ba77d5c30b4
avril-m-harder <avrilharder@gmail.com>
2025-10-08 16:59:08 -0500

    update

--- scripts/templates/02_vg_combine_and_haplos.TEMPLATE.sh
+++ scripts/templates/02_vg_combine_and_haplos.TEMPLATE.sh
@@ -83,47 +83,60 @@ fi
 # https://github.com/vgteam/vg/wiki/Haplotype-Sampling
 # -----------------------------------------------------------------------------
 
-echo "building GBZ and XG from GFA - " $(TZ=${ZONE} date) >> ${LOGFILE}
-if [ ! -f ${INDIR}/${GBZ} ]; then
+echo "building GBZ from GFA - " $(TZ=${ZONE} date) >> ${LOGFILE}
+if ! [[ -f ${INDIR}/${GBZ} ]] || ! [[ -f ${INDIR}/${XG} ]]; then
 
 
 		## writes GBZ from GFA
+		if [ ! -f ${INDIR}/${GBZ} ]; then
 		apptainer exec ${VG_IMAGE} \
 		vg gbwt \
 			--gbz-format \
+			--num-threads ${VG_HAP_NTHREADS} \
 			-g ${GBZ} \
 			-G ${GFA}
+		fi
 		
 		## writes path metadata to text file
+		if [ ! -f ${GRAPH_STATS_DIR}/${GBZ}.pathstats.txt ]; then
 		apptainer exec ${VG_IMAGE} \
 		vg paths \
 			-x ${GBZ} \
+			--threads ${VG_HAP_NTHREADS} \
 			-M > \
 			${GRAPH_STATS_DIR}/${GBZ}.pathstats.txt
+		fi
 
 		## writes reference path as FASTA
+		if [ ! -f ${INDIR}/${REF_PATH_FA_BGZIP} ]; then
 		apptainer exec ${VG_IMAGE} \
 		vg paths \
 			-x ${GBZ} \
 			--extract-fasta \
-			--sample ${PRIMREF} |
+			--threads ${VG_HAP_NTHREADS} \
+			--sample ${PRIMREF} | \
 			bgzip > \
 			${REF_PATH_FA_BGZIP}
+		fi
 
 		## writes XG from GFA
+		if [ ! -f ${INDIR}/${XG} ]; then
 		apptainer exec ${VG_IMAGE} \
 		vg convert \
 			-g ${GFA} \
-			-x ${XG}
+			--threads ${VG_HAP_NTHREADS} \
+			-x > \
+			${XG}
+		fi
 		
 		rsync -avuP ${XG} ${INDIR}	
 		rsync -avuP ${XG} ${STMVIZ_DIR}	
 		rsync -avuP ${GBZ}* ${INDIR}
-		rsync -avuP ${REF_PATH_FA} ${INDIR}
+		rsync -avuP ${REF_PATH_FA_BGZIP} ${INDIR}
 
 else
 	
-	rsync -avuP ${INDIR}/${GBZ} .
+	rsync -avuP ${INDIR}/${GBZ}* .
 
 fi
 
@@ -136,27 +149,23 @@ if [ ! -f ${INDIR}/${HAPL} ]; then
 	echo "${line[0]} - building giraffe indices - " $(TZ=${ZONE} date) >> ${LOGFILE}
 	
 	## writes distance index needed for writing haplotype index
+	if [ ! -f ${INDIR}/${DIST} ]; then				
 	apptainer exec ${VG_IMAGE} \
 	vg index \
 		-t ${VG_HAP_NTHREADS} \
 		--dist-name ${DIST} \
 		--no-nested-distance \
 		${GBZ}
+	fi
 
 	## writes r-index needed for writing haplotype index
+	if [ ! -f ${INDIR}/${RI} ]; then				
 	apptainer exec ${VG_IMAGE} \
 	vg gbwt \
 		--num-threads ${VG_HAP_NTHREADS} \
 		--gbz-input ${GBZ} \
 		--r-index ${RI}
-
-	## writes minimizer index needed for writing haplotype index
-	apptainer exec ${VG_IMAGE} \
-	vg minimizer \
-		-d ${DIST} \
-		-t ${VG_HAP_NTHREADS} \
-		-o ${MIN} \
-		${GBZ}
+	fi
 		
 	## writes graph haplotype info for subgraph construction
 	apptainer exec ${VG_IMAGE} \
@@ -187,30 +196,52 @@ echo "running summary scripts - " $(TZ=${ZONE} date) >> ${LOGFILE}
 cat $(find ${INDIR} -name "minigraph.split.log") > \
 ${GRAPH_STATS_DIR}/minigraph_split_logs.txt
 
+## horizontal bar/line plots of proportion retained per sample, 1 plot per chromosome
 Rscript --vanilla \
 	${SCRIPT_DIR}/R01_chrom_coverage_stats.R \
 	${GRAPH_STATS_DIR}/minigraph_split_logs.txt \
-	${GRAPH_STATS_PLOTS}/${TAXON}_chrom_coverage_props.pdf
+	${GRAPH_STATS_PLOTS}/${TAXON}_chrom_coverage_props.pdf \
+	${TAXON}
+	
+## dot plots of proportion retained by chromosome and by sample (2 plots)
+## get coordinates of sequences kept as subpaths for each input sample (haplotype)
+grep -e '^W' ${GFA} | \
+cut -f2-6 | \
+awk '{ print $1 "\t" $3 "\t" $4 "\t" $5 }' | \
+awk '{ print $2 "\t" $3 "\t" $4 >$1"_subpath_included_coords.bed" }'
+for SAMP in $(cut -f1 ${INFO_DIR}/cactus_CHROM_seqfile.txt)
+do
+	SAMP1=$(sed 's/[^[:alnum:]]/*/g' <<< "$SAMP")
+	if [ -f ${REF_DIR}/*${SAMP1}*chr_only*.fai ]; then
+		FAI=$(ls ${REF_DIR}/*${SAMP}*chr_only*.fai)
+	else
+		FAI=$(ls ${REF_DIR}/*${SAMP1}*.fai)
+	fi
+	cut -f1,2 ${FAI} > ${SAMP}.genome
+done
+Rscript --vanilla \
+	${SCRIPT_DIR}/R03_plotting_clipped_by_chrom_and_samp.R \
+	${TAXON}
 	
 ## Panacus plots: can be customized by editing ${INFO_DIR}/panacus_report.yaml
-base=$(basename ${GFA} .gfa)
+BASE=$(basename ${GFA} .gfa)
 
 panacus report \
 	${INFO_DIR}/panacus_report.yaml > \
 	${GRAPH_STATS_PLOTS}/${BASE}_panacus_report.html
 
+## Plotting coverage of samples x chromosomes by representation in the 
+## final (clipped) graph - chromosome diagram format
+
 grep -e '^W' ${GFA} | \
 cut -f2-6 | \
 awk '{ print $1 "#" $2 "#" $3 ":" $4 "-" $5 }' > \
-${base}.paths.txt
+${BASE}.paths.txt
 
-## Plotting coverage of samples x chromosomes by representation in the 
-## final (clipped) graph
 Rscript --vanilla \
 	${SCRIPT_DIR}/R02_chrom_coverage_graphics.R \
-	./${base}.paths.txt \
-	${REF_DIR} \
-	${GRAPH_STATS_PLOTS}/${base}_chrom_coverage_graphic.pdf
+	./${BASE}.paths.txt \
+	${GRAPH_STATS_PLOTS}/${BASE}_chrom_coverage_graphic.pdf
 
 echo "complete - " $(TZ=${ZONE} date) >> ${LOGFILE}
 
